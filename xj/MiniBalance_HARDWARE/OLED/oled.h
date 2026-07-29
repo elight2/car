e1fe84dc24d5cda3f92e4df14fe8be1f13b6178d
jing <you@example.com>
2026-07-29 10:44:05 +0800

    m

--- xj/MiniBalance_HARDWARE/OLED/oled.h
+++ xj/MiniBalance_HARDWARE/OLED/oled.h
@@ -1,18 +1,18 @@
 /***********************************************
-公司：轮趣科技(东莞)有限公司
-品牌：WHEELTEC
-官网：wheeltec.net
-淘宝店铺：shop114407458.taobao.com 
-速卖通: https://minibalance.aliexpress.com/store/4455017
-版本：V1.0
-修改时间：2022-09-05
+锟斤拷司锟斤拷锟斤拷趣锟狡硷拷(锟斤拷莞)锟斤拷锟睫癸拷司
+品锟狡ｏ拷WHEELTEC
+锟斤拷锟斤拷锟斤拷wheeltec.net
+锟皆憋拷锟斤拷锟教ｏ拷shop114407458.taobao.com 
+锟斤拷锟斤拷通: https://minibalance.aliexpress.com/store/4455017
+锟芥本锟斤拷V1.0
+锟睫革拷时锟戒：2022-09-05
 
 Brand: WHEELTEC
 Website: wheeltec.net
 Taobao shop: shop114407458.taobao.com 
 Aliexpress: https://minibalance.aliexpress.com/store/4455017
 Version: V1.0
-Update：2022-09-05
+Update锟斤拷2022-09-05
 
 All rights reserved
 ***********************************************/
@@ -24,27 +24,27 @@ All rights reserved
 
 
 
-/* 定义OLED连接的GPIO端口, 用户只需要修改下面的代码即可改变控制的OLED引脚 */
-#define OLED_SCLK_GPIO_PORT    	GPIOC			              /* GPIO端口 */
-#define OLED_SCLK_GPIO_CLK 	    RCC_APB2Periph_GPIOC		/* GPIO端口时钟 */
-#define OLED_SCLK_GPIO_PIN		GPIO_Pin_14			        /* 相应引脚号 */
+/* 4寮曡剼 OLED 鎺у埗绾垮畾涔夛細SCL / SDA / RST / DC */
+#define OLED_SCLK_GPIO_PORT    	GPIOC				      /* GPIO绔彛 */
+#define OLED_SCLK_GPIO_CLK 	    RCC_APB2Periph_GPIOC		/* GPIO绔彛鏃堕挓 */
+#define OLED_SCLK_GPIO_PIN		GPIO_Pin_14			        /* SCL */
 
-#define OLED_SDIN_GPIO_PORT    	GPIOB			              /* GPIO端口 */
-#define OLED_SDIN_GPIO_CLK 	    RCC_APB2Periph_GPIOB		/* GPIO端口时钟 */
-#define OLED_SDIN_GPIO_PIN		GPIO_Pin_5			        /* 相应引脚号 */
+#define OLED_SDIN_GPIO_PORT    	GPIOB				      /* GPIO绔彛 */
+#define OLED_SDIN_GPIO_CLK 	    RCC_APB2Periph_GPIOB		/* GPIO绔彛鏃堕挓 */
+#define OLED_SDIN_GPIO_PIN		GPIO_Pin_5			        /* SDA */
 
-#define OLED_RST_GPIO_PORT    	GPIOB			              /* GPIO端口 */
-#define OLED_RST_GPIO_CLK 	    RCC_APB2Periph_GPIOB		/* GPIO端口时钟 */
-#define OLED_RST_GPIO_PIN		GPIO_Pin_4			        /* 相应引脚号 */
+#define OLED_RST_GPIO_PORT    	GPIOB				      /* GPIO绔彛 */
+#define OLED_RST_GPIO_CLK 	    RCC_APB2Periph_GPIOB		/* GPIO绔彛鏃堕挓 */
+#define OLED_RST_GPIO_PIN		GPIO_Pin_4			        /* RST */
 
-#define OLED_RS_GPIO_PORT    	GPIOB			              /* GPIO端口 */
-#define OLED_RS_GPIO_CLK 	    RCC_APB2Periph_GPIOB		/* GPIO端口时钟 */
-#define OLED_RS_GPIO_PIN		GPIO_Pin_3			        /* 相应引脚号 */
-
-
+#define OLED_RS_GPIO_PORT    	GPIOB				      /* GPIO绔彛 */
+#define OLED_RS_GPIO_CLK 	    RCC_APB2Periph_GPIOB		/* GPIO绔彛鏃堕挓 */
+#define OLED_RS_GPIO_PIN		GPIO_Pin_3			        /* DC */
 
 
-//-----------------OLED端口定义---------------- 
+
+
+//-----------------OLED锟剿口讹拷锟斤拷---------------- 
 #define OLED_RST_Clr() PBout(4)=0   //RST
 #define OLED_RST_Set() PBout(4)=1   //RST
 
@@ -57,8 +57,8 @@ All rights reserved
 #define OLED_SDIN_Clr()  PBout(5)=0   //SDA
 #define OLED_SDIN_Set()  PBout(5)=1   //SDA
 
-#define OLED_CMD  0	//写命令
-#define OLED_DATA 1	//写数据
+#define OLED_CMD  0	//写锟斤拷锟斤拷
+#define OLED_DATA 1	//写锟斤拷锟斤拷
 
 
 
@@ -66,7 +66,7 @@ All rights reserved
 
 extern u8 OLED_GRAM[128][8];	 
 
-//OLED控制用函数
+//OLED锟斤拷锟斤拷锟矫猴拷锟斤拷
 void OLED_WR_Byte(u8 dat,u8 cmd);	    
 void OLED_Display_On(void);
 void OLED_Display_Off(void);
